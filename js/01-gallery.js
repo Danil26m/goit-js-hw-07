4a8ca5c091e2c8f677e2d035ffc477edfeea3263
Danil26m <111749790+Danil26m@users.noreply.github.com>
2022-10-27 16:44:39 +0300

    m

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const divv =  document.querySelector(".gallery");
 
-console.log(galleryItems);
+
+function addImg(img) {
+  img.map(el => divv.insertAdjacentHTML('afterbegin',`
+  <div class="gallery__item">
+  <a class="gallery__link" href="${el.original}">
+    <img
+      class="gallery__image"
+      src="${el.preview}"
+      data-source="${el.original}"
+      alt="${el.description}"
+    />
+  </a>
+</div>`))
+return img;
+}
+
+function img(elem) {
+  elem.preventDefault();
+  if (!elem.target.classList.contains('gallery__image')) {
+    return;
+  }
+  const instance = basicLightbox.create(`
+  <img src=${elem.target.dataset.source} width="auto" height="auto">
+`)
+instance.show()
+console.log(elem.target.dataset.source);
+}
+  divv.addEventListener("click", img)
+addImg(galleryItems);
+console.log(addImg(galleryItems))
